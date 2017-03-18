824be9e9d5647255f5a3b5cd7f8cb3f4a8ed7b4a
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2017-03-18 15:01:44 -0600

    commits for 3/18/2017

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,17 +3,20 @@
  
                  var points = document.getElementsByClassName('point');
                   
-                 var revealPoint = function(diffPoints) {
-                     points[diffPoints].style.opacity = 1;
-                     points[diffPoints].style.transform = "scaleX(1) translateY(0)";
-                     points[diffPoints].style.msTransform = "scaleX(1) translateY(0)";
-                     points[diffPoints].style.WebkitTransform = "scaleX(1) translateY(0)";
+                 var revealPoint = function(index) {
+                     points[index].style.opacity = 1;
+                     points[index].style.transform = "scaleX(1) translateY(0)";
+                     points[index].style.msTransform = "scaleX(1) translateY(0)";
+                     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+                     
+                  }
                  
                  for(var i = 0; i < points.length; i++){
                     
                      revealPoint(i);
-                }
+              
                   }
+            };
 
           
         
