0794ea3d27cae138c3c4927b6c271f00f4b49ed8
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2017-04-13 23:25:49 -0600

    hopefully final commits to checkpoint 16

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
-var animatePoints = function(){
-    var revealPoint = function(){
+var animatePoints = function() {
+    var revealPoint = function() {
         $(this).css({        
-            opacity: 1;
+            opacity: 1,
             transform: 'scaleX(1) translateY(0)'    
         });  
     };
@@ -19,10 +19,10 @@ $(window).load(function() {
     $(window).scroll(function(event) {
         if ($(window).scrollTop() >= scrollDistance) {
             animatePoints();  
-    }
+        }
                 
+    });
+
 });
-        
-}
           
         
